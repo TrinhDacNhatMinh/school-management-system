729ba8b09b66e2181772f713578871f87ad470b4
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-10-10 00:12:39 +0700

    Refine teacher service interface

--- src/main/java/com/minh/school_management_system/service/TeacherService.java
+++ src/main/java/com/minh/school_management_system/service/TeacherService.java
@@ -1,20 +1,20 @@
 package com.minh.school_management_system.service;
 
 import com.minh.school_management_system.dto.request.TeacherRequest;
+import com.minh.school_management_system.dto.request.UpdatedTeacherRequest;
 import com.minh.school_management_system.dto.response.TeacherResponse;
 
 import java.util.List;
 
 public interface TeacherService {
 
-    // CRUD
     public TeacherResponse createTeacher(TeacherRequest request);
 
     public List<TeacherResponse> getAllTeachers();
 
     public TeacherResponse getTeacherById(Long id);
 
-    public TeacherResponse updateTeacher(Long id, TeacherRequest request);
+    public void updateTeacher(Long teacherId, UpdatedTeacherRequest request);
 
     public void deleteTeacher(Long id);
 

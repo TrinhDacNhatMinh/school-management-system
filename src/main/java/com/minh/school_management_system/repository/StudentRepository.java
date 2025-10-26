5ebc53dde755885a558a34bdb465ce141bab63cf
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-10-26 10:01:46 +0700

    feat(student): add repository method to find student by associated user username

--- src/main/java/com/minh/school_management_system/repository/StudentRepository.java
+++ src/main/java/com/minh/school_management_system/repository/StudentRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Query("SELECT MAX(SUBSTRING(s.studentCode, LENGTH(:key) + 1, 3)) FROM Student s WHERE s.studentCode LIKE CONCAT(:key, '%')")
     public Integer findMaxSequenceByKey(@Param("key") String key);
+
+    public Optional<Student> findByUser_Username(String username);
 
 }
